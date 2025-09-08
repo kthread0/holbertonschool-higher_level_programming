f8ee1f7a800d15d24f3b7de4db539bb81c178717
kthread0 <dev@kthread.dev>
2025-09-07 23:32:49 -0300

    fixed

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-
 def uppercase(str):
+    result = ""
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            print("{}".format(chr(ord(str[i]) - 32)), end="")
+            result += chr(ord(str[i]) - 32)
         else:
-            print("{}".format(str[i]), end="")
-    print()
+            result += str[i]
+    print("{}".format(result))
