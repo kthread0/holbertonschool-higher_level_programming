1503d372478ec98533b76e0aebae5b66dd0c27c6
kthread0 <dev@kthread.dev>
2025-09-07 23:42:18 -0300

    finished

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, subtract, multiply, divide
+
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, subtract(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, multiply(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, divide(a, b)))
