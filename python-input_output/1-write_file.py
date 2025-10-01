dcf8a3387e10d3ab54389e08aa6c8a91e7dae147
kthread0 <dev@kthread.dev>
2025-10-01 11:47:28 -0300

    fix: improve formatting of docstring in write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,7 +6,10 @@ Write a string to a text file
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file and returns the number of characters written"""
+    """
+    Writes a string to a text file and
+    returns the number of characters written
+    """
     with open(filename, "w") as file:
         return file.write(text)
     return None
