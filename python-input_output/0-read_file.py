285a9d725f38aefea30c08641508c3a5bb01cfae
kthread0 <dev@kthread.dev>
2025-10-01 11:38:04 -0300

    refactor: remove redundant docstring from read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,6 @@ Module that defines a function to read a text file
 """
 
 def read_file(filename=""):
-    """Reads a text file"""
     with open(filename) as file:
         print(file.read(), end="")
     return None
