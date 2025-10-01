f3da7a7455b90af9c7212500f45f9b7798494d07
kthread0 <dev@kthread.dev>
2025-10-01 11:45:07 -0300

    add: implement write_file function to write a string to a text file

---
+++ python-input_output/0-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Write a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file and returns the number of characters written"""
+    with open(filename, "w") as file:
+        return file.write(text)
+    return None
