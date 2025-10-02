bac7a6a44074e7f43eb6dd6f2a4c605b1af78cd9
kthread0 <dev@kthread.dev>
2025-10-02 13:15:15 -0300

    add documentation

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
+"""
+Convert a JSON string to a Python object.
+Returns:
+    dict: The Python object representation of the JSON string.
+"""
+
 import json
 
+
 def from_json_string(my_dict):
+    """
+    :param my_dict: Description
+    """
     return json.loads(my_dict)
